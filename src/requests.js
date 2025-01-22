e6282b84394ebd75a9d4a766eb3830ccc2394628
Igor Martins <igorjosemartins01@gmail.com>
2025-01-21 23:08:34 -0300

    retirado requisição de checagem se usuário encontrou partida

--- src/requests.js
+++ src/requests.js
@@ -1,11 +1,3 @@
-export const checkIfFoundMatch = async (https) => {
-  const request = https.build('/lol-matchmaking/v1/ready-check')
-    .method('get')
-    .create();
-
-  return await request();
-};
-
 export const acceptMatch = async (https) => {
   const request = https.build('/lol-matchmaking/v1/ready-check/accept')
     .method('post')
